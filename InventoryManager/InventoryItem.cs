635761817e76159dbaee87d53415d53ccdb3313b
13maffine <nikolajgluskov6807@gmail.com>
2025-11-11 21:34:28 +0300

    Добавлен новый классы

---
+++ InventoryManager/InventoryItem.cs
@@ -0,0 +1,17 @@
+﻿namespace InventoryManagerNmspc
+{
+    public class InventoryItem
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public InventoryItem(string name, int quantity, decimal price, string category)
+        {
+            Name = name;
+            Quantity = quantity;
+            Price = price;
+            Category = category;
+        }
+    }
+}
